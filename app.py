4fcef7203c4c8cc9233687b2f44d77a119f7514d
jaysaville <jaylon@hey.com>
2022-11-23 15:57:09 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import streamlit as st
+import cv2 as cv
+import tempfile
+from google.cloud import storage
+
+st.header("Dance Synchronisation")
+
+#Receive video file from user upload
+uploaded_video = st.file_uploader("**Upload video for evaluation**", ['mp4', 'gif'], key='dance')
+if uploaded_video is not None:
+    #Convert from bytesio to cv
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(uploaded_video.read())
+    vf = cv.VideoCapture(tfile.name)
+    #Upload to google bucket
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("dance-sync-user-upload")
+    blob = bucket.blob("user_upload")
+    blob.upload_from_filename(vf)
