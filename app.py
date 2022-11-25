989f7b815e7673125d7b83cc3f72b1509076920b
jaysaville <jaylon@hey.com>
2022-11-25 12:04:04 +1100

    added timeframe buttons

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import matplotlib as plt
 import seaborn as sns
 import numpy as np
 import time
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 from google.cloud import storage
 from google.oauth2 import service_account
 
@@ -33,11 +35,30 @@ def get_file(source_blob, save_as, source_bucket=bucket):
             pass
     return result
 
+#animations
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 def main():
 
-    st.set_page_config(page_title="Dance Synchronisation")
-    st.header("Dance Synchronisation")
+    st.set_page_config(layout="wide", page_title="Dance Synchronisation")
+
+    #load animations
+    lottie_url_dancing = "https://assets7.lottiefiles.com/packages/lf20_owg7kezi.json"
+    lottie_dancing = load_lottieurl(lottie_url_dancing)
+
+    #build header
+    col1, col2, col3 = st.columns([1,2,1])
+    with col1:
+        st_lottie(lottie_dancing, key="dance_left")
+    with col2:
+        st.markdown("<h1 style='text-align: center; color: yellow;'>Dance Synchronisation</h1>", unsafe_allow_html=True)
+    with col3:
+        st_lottie(lottie_dancing, key="dance_right")
+
 
     #Receive video file from user upload
     uploaded_video = st.file_uploader("**Upload video for evaluation**", ['mp4', 'gif'], key='dance')
@@ -46,7 +67,7 @@ def main():
     if uploaded_video is not None:
 
         with st.spinner('Contacting api...'):
-            url = "https://syncv2-eagwezifvq-an.a.run.app/vid_process_from_st"
+            url = "https://syncv3-eagwezifvq-an.a.run.app/vid_process_from_st"
             files = {"file": (uploaded_video.name, uploaded_video, "multipart/form-data")}
             response = requests.post(url, files=files).json()
         st.write(response)
@@ -60,15 +81,39 @@ def main():
             #Retrieve video
             if not get_file(source_blob='video.mp4', save_as='video.mp4'):
                 st.error("Video request timeout")
-        st.balloons()
 
         #Plot results
-        st.line_chart(df)
+        df['Sync'] = df['Sync'] - 0.5
+        st.area_chart(data=df, x='Time', y='Sync', )
 
-        #Display video
+        #Timestamp buttons
+        sorted_df = df.sort_values(by=['Sync']).round(2).head(5)
+        timestamps = sorted_df['Time'].to_list()
+        sync = sorted_df['Sync'].to_list()
+        # button1, button2, button3, button4, button5 = st.columns(5)
+        # with button1:
+        #     if st.button('1'):
+        st.write("**Your top five miss-steps:**")
+        # bool, moment, description
+        button_col, video_col = st.columns(2)
+        with button_col:
+            buttons = [
+                (st.button("A", "a"), timestamps[0], st.write(f'Time: {timestamps[0]}s, Sync: {sync[0]}')),
+                (st.button("B", "b"), timestamps[1], st.write(f'Time: {timestamps[1]}s, Sync: {sync[1]}')),
+                (st.button("C", "c"), timestamps[2], st.write(f'Time: {timestamps[2]}s, Sync: {sync[2]}')),
+                (st.button("D", "d"), timestamps[3], st.write(f'Time: {timestamps[3]}s, Sync: {sync[3]}')),
+                (st.button("E", "e"), timestamps[4], st.write(f'Time: {timestamps[4]}s, Sync: {sync[4]}'))
+            ]
+        #load video data
         video_file = open('video.mp4', 'rb')
         video_bytes = video_file.read()
-        st.video(video_bytes)
+        #empty placeholder to reload widget when different button is pressed
+        with video_col:
+            placeholder = st.empty()
+            time_chosen = [v for k, v, t in buttons if k == True]  # return time associated with a clicked button
+            if time_chosen:
+                placeholder.empty()
+                placeholder.video(video_bytes, start_time=time_chosen[0])
 
 if __name__ == '__main__':
 	main()
